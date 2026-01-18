468a81ebfcb2f903a9d27aef6429aadefb0090f4
fangorntb <belyakov.kirill.kirill@yandex.ru>
2026-01-18 23:54:51 +0300

    Тайпинги для методов

--- src/core/services/chat.py
+++ src/core/services/chat.py
@@ -1,6 +1,9 @@
+from typing import Tuple, List
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.services.base import BaseService
+from src.infrastructure.db.models import Chat, Message
 from src.infrastructure.db.repositories import ChatRepository, MessageRepository
 from src.core.exceptions.chat import ChatNotFoundError
 
@@ -11,16 +14,16 @@ class ChatService(BaseService):
         self.chat_repo = ChatRepository(session)
         self.message_repo = MessageRepository(session)
 
-    async def create_chat(self, title: str):
+    async def create_chat(self, title: str) -> Chat:
         return await self.chat_repo.create(title=title)
 
-    async def _get_chat_or_fail(self, chat_id: int):
+    async def _get_chat_or_fail(self, chat_id: int) -> Chat:
         chat = await self.chat_repo.get_by_id(chat_id)
         if not chat:
             raise ChatNotFoundError()
         return chat
 
-    async def get_chat_with_messages(self, chat_id: int, limit: int):
+    async def get_chat_with_messages(self, chat_id: int, limit: int) -> Tuple[Chat, List[Message]]:
         chat = await self._get_chat_or_fail(chat_id)
         messages = await self.message_repo.get_last_by_chat(
             chat_id=chat_id,
@@ -28,14 +31,14 @@ class ChatService(BaseService):
         )
         return chat, messages
 
-    async def send_message(self, chat_id: int, text: str):
+    async def send_message(self, chat_id: int, text: str) -> Message:
         await self._get_chat_or_fail(chat_id)
         return await self.message_repo.create(
             chat_id=chat_id,
             text=text,
         )
 
-    async def delete_chat(self, chat_id: int):
+    async def delete_chat(self, chat_id: int) -> None:
         deleted = await self.chat_repo.delete_by_id(chat_id)
         if not deleted:
             raise ChatNotFoundError()
